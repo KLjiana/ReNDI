65fdc5a641346847054d1bc8cd9f512f7f550636
KL_jiana <2436262516@qq.com>
2025-01-01 11:20:42 +0800

    🧢version update 🔧refmap fix

--- src/main/java/net/profhugo/nodami/mixin/LivingEntityMixin.java
+++ src/main/java/net/profhugo/nodami/mixin/LivingEntityMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
